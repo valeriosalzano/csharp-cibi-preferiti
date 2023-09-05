94e9f4d19abb1f3097076e6051c04ec077284916
Valerio Salzano <vlr.slzn@gmail.com>
2023-09-05 14:44:39 +0200

    Add project files.

---
+++ csharp-cibi-preferiti/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+string[] favourite_foods = 
+{ 
+    "pizza",
+    "pasta al sugo",
+    "carne alla brace",
+    "sushi",
+    "cheesecake",
+    "carbonara",
+};
+
+Console.WriteLine(favourite_foods.Length);
