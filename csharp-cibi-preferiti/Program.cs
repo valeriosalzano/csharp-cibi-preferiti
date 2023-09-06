0649f476d88361f09090a2be34b8d25bd292f1b3
Valerio Salzano <vlr.slzn@gmail.com>
2023-09-06 10:51:53 +0200

    variables name fixed (replaced underscore with camel case)

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-string[] favourite_dishes = 
+string[] favouriteDishes = 
 { 
     "pizza",
     "carne alla brace",
@@ -8,34 +8,37 @@ string[] favourite_dishes =
     "cheesecake",
     "carbonara",
     "pasta al sugo", //commentare per lista dispari
+    "porchetta",
+    "prova",
+    "prova prova",
 };
 
 // numero di elementi
-Console.WriteLine("La lista contiene " + favourite_dishes.Length + " elementi.");
+Console.WriteLine("La lista contiene " + favouriteDishes.Length + " elementi.");
 
 // classifica intera
 Console.WriteLine("Ecco la classifica:");
-for (int i = 0; i < favourite_dishes.Length; i++)
+for (int i = 0; i < favouriteDishes.Length; i++)
 {
-    Console.WriteLine($"{i + 1} - {favourite_dishes[i]}");
+    Console.WriteLine($"{i + 1} - {favouriteDishes[i]}");
 }
 
 // primo in lista
-Console.WriteLine("Il mio cibo preferito in assoluto: " + favourite_dishes[0]);
+Console.WriteLine("Il mio cibo preferito in assoluto: " + favouriteDishes[0]);
 
 // ultimo in lista
-Console.WriteLine("Un mio cibo preferito (ma non troppo): " + favourite_dishes[(favourite_dishes.Length - 1)]);
+Console.WriteLine("Un mio cibo preferito (ma non troppo): " + favouriteDishes.Last());
 
 // *** Bonus ***
 
 Console.Write("A metà della classifica abbiamo ");
 
-int list_length = favourite_dishes.Length;
+int listLength = favouriteDishes.Length;
 
-if (list_length % 2 == 0)
+if (listLength % 2 == 0)
 {
-    Console.WriteLine($"a pari merito: {favourite_dishes[(list_length / 2) - 1]} e {favourite_dishes[list_length / 2]}.");
+    Console.WriteLine($"a pari merito: {favouriteDishes[(listLength / 2) - 1]} e {favouriteDishes[listLength / 2]}.");
 } else
 {
-    Console.WriteLine(": " + favourite_dishes[list_length / 2]);
+    Console.WriteLine(": " + favouriteDishes[listLength / 2]);
 }
